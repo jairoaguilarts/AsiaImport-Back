2aab2180b79541e64c8e8c4e3b0747b208c3fd0e
BoroaDev7 <jmborjas@unitec.edu>
2024-02-04 22:54:24 -0600

    Cambios de estado Pago

--- schemas/ordenSchema.js
+++ schemas/ordenSchema.js
@@ -3,54 +3,50 @@ const Entrega = require("./entregaSchema");
 const { type } = require("os");
 
 const ordenSchema = new mongoose.Schema({
-    nombre_usuario: {
-        type: String,
-        required: true,
-    },
-    firebaseUID: {
-        type: String,
-        required: true,
-    },
-    correo: {
-        type: String,
-       
-    },
-    tipoOrden: {
-        type: String,
-        required: true,
-    },
-    carrito: {
-        type: [String],
-        default: [],
-        required: true,
-    },
-    cantidades: {
-        type: [String],
-        default: [],
-    },
-    detalles: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Entrega",
-        required: true,
-    },
-    estadoOrden: {
-        type: String,
-        required: true,
-    },
-    estadoPago: {
-        type: String,
-        required: true,
-    },
-    detallePago: {
-        type: String
-    },
-    total: {
-        type: String,
-    },
-    Fecha: {
-        type: String,
-        required: true,
-    },
+  nombre_usuario: {
+    type: String,
+    required: true,
+  },
+  firebaseUID: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+  },
+  tipoOrden: {
+    type: String,
+    required: true,
+  },
+  carrito: {
+    type: [String],
+    default: [],
+    required: true,
+  },
+  cantidades: {
+    type: [String],
+    default: [],
+  },
+  detalles: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Entrega",
+    required: true,
+  },
+  estadoOrden: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: String,
+  },
+  Fecha: {
+    type: String,
+    required: true,
+  },
+  estadoPago: {
+    type: String,
+    required: true,
+  },
 });
 
 const ordenes = mongoose.model("ordenes", ordenSchema, "ordenes");
--- schemas/pickupSchema.js
+++ schemas/pickupSchema.js
@@ -1,30 +1,34 @@
 const mongoose = require("mongoose");
 
 const pickupSchema = new mongoose.Schema({
-    nombreUser: {
-        type: String,
-        required: true,
-    },
-    identidadUser: {
-        type: String,
-        required: true,
-    },
-    id_usuario: {
-        type: String,
-        required: true,
-    },
-    estadoOrden: {
-        type: String,
-        required: true,
-    },
-    fecha_ingreso: {
-        type: String,
-        required: true,
-    },
-    numerotelefono: {
-        type: String,
-        required: true,
-    },
+  nombreUser: {
+    type: String,
+    required: true,
+  },
+  identidadUser: {
+    type: String,
+    required: true,
+  },
+  id_usuario: {
+    type: String,
+    required: true,
+  },
+  estadoOrden: {
+    type: String,
+    required: true,
+  },
+  fecha_ingreso: {
+    type: String,
+    required: true,
+  },
+  numerotelefono: {
+    type: String,
+    required: true,
+  },
+  //   estadoPago: {
+  //     type: String,
+  //     required: true,
+  //   },
 });
 
 const pickups = mongoose.model("pickup", pickupSchema);
